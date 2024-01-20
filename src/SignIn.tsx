e9148877c2d51c20761c534a2be705f9f19bb990
Callum Mackenzie <callum.alex.mackenzie@gmail.com>
2024-01-20 12:36:23 -0800

    Add mui

--- src/SignIn.tsx
+++ src/SignIn.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Auth } from "firebase/auth";
+import { GoogleAuthProvider } from 'firebase/auth';
+import { Button, Container } from '@mui/material';
 
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+	'login_hint': "user@example.com"
+});
 
 export const SignIn = (props: {
 	auth: Auth
 }) => {
 	return (<>
-		<p>
-			SIGN IN
-		</p>
+		<Container>
+			<Button variant='contained'>
+				Sign In With Google
+			</Button>
+		</Container>
 	</>);
 }
