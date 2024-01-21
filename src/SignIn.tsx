f4e148419ebb73e082c67f124ceeb0c86b31416b
Callum Mackenzie <callum.alex.mackenzie@gmail.com>
2024-01-21 01:35:36 -0800

    Add copyright

--- src/SignIn.tsx
+++ src/SignIn.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Auth } from "firebase/auth";
-import { Box, Button } from "@mui/material";
+import { Box, Button, Grid } from "@mui/material";
 import { NavigateFunction, redirect, useNavigate } from "react-router-dom";
 import { signInGoogle, useSignIn } from "./UseSignIn";
 import vitAlertImage from "./images/VitAlert-Full.png";
@@ -15,22 +15,30 @@ export const SignIn = (props: { auth: Auth }) => {
 	}, [foundUser]);
 
 	return (
-		<Stack alignItems="center" justifyContent="center">
-			<img
-				src={vitAlertImage}
-				alt="VITALERT"
-				style={{
-					objectFit: 'contain',
-					height: "70vh",
-					maxWidth: "95%",
-				}}
-			/>
-			<Button
-				variant="contained"
-				disabled={foundUser}
-				onClick={() => signInGoogle(props.auth)}>
-				Sign In With Google
-			</Button>
+		<Stack direction={'column'}
+			textAlign={'center'}
+			alignContent={'space-around'}
+			alignItems={'center'}>
+			<Stack alignItems="center" justifyContent="center" textAlign={'center'} paddingBottom={10}>
+				<img
+					src={vitAlertImage}
+					alt="VITALERT"
+					style={{
+						objectFit: 'contain',
+						height: "70vh",
+						maxWidth: "95%",
+					}}
+				/>
+				<Button
+					variant="contained"
+					disabled={foundUser}
+					onClick={() => signInGoogle(props.auth)}>
+					Sign In With Google
+				</Button>
+			</Stack>
+			<span>
+				Copyright © 2024 Callum Mackenzie, Fegico Chen, Terence Yin, Hieu Le
+			</span>
 		</Stack>
 	);
 };
