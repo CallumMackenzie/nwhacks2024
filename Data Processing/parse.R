d4fb4a028955d254c98baaf9c81744206de2aed0
Callum Mackenzie <callum.alex.mackenzie@gmail.com>
2024-01-20 17:14:45 -0800

    Parsed symptom data with R

--- Data Processing/parse.R
+++ Data Processing/parse.R
@@ -3,9 +3,13 @@ library(dplyr)
 library(RJSONIO)
 
 rdi = read_tsv("./rdi.tsv")
-
 rdi$PregnantWomen
-
 json <- toJSON(rdi)
-
 write.table(json, file = "rdi.json", row.names = F, quote=F, col.names = F)
+
+
+symptoms <- read_tsv("./symptoms.tsv")
+symptoms$Nutrient
+symptoms$Function
+symptoms$Symptoms
+
