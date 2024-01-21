873a922b778b282facbc0defbf1d23731e214a87
Callum Mackenzie <callum.alex.mackenzie@gmail.com>
2024-01-21 09:45:18 -0800

    Fix merge isssue in nutrient view

--- src/NutrientView.tsx
+++ src/NutrientView.tsx
@@ -45,14 +45,6 @@ export const NutrientView = (props: {
 
 
 	// const nutrient = "ALPHA LIPOIC ACID";
-	let index = -1;
-	for (let i = 0; i < symptomsJson.Name.length; i++) {
-		if (symptomsJson.Name[i] === nutrient) {
-			index = i;
-			break;
-		}
-	}
-	if (index == -1)
 	//const nutrient = "VITAMIN A";
 	const data = getSymptomData(nutrient ?? "");
 	if (data === undefined)
@@ -62,16 +54,13 @@ export const NutrientView = (props: {
 				<Button onClick={() => navigate("/home")}>Home</Button>
 			</Stack>
 		</>);
-	const rarity = symptomsJson.Rarity[index];
-	const func = symptomsJson.Function[index];
-	const symp = symptomsJson.Symptoms[index];
-	const source = symptomsJson.Sources[index];
+	const { name, rarity, func, sources, symptoms } = data;
 	let itemData: any = [];
-	let sourceList = source.split(",");
+	let sourceList = sources.split(",");
 	sourceList = sourceList.map((data) => (data.trim().toLowerCase()));
 	console.log(sourceList);
 	sourceList.forEach((item) => {
-		if (Object.hasOwn(foodJson,item)){
+		if (Object.hasOwn(foodJson, item)) {
 			itemData.push(foodJson[item as keyof typeof foodJson]);
 		}
 	})
@@ -89,39 +78,39 @@ export const NutrientView = (props: {
 	else {
 		colorString = "red";
 	}
-	
-  	return (
-    <>
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', paddingTop: '50px' }}>
-        <Card variant="outlined" style={{ margin: 'auto', maxWidth: 600 }}>
-          	<CardContent>
-            	<Typography variant="h2" component="div" align="center" gutterBottom style={{ fontSize: '3.0rem' }}>
-              		{nutrient}
-            	</Typography>
-            	<Typography color={colorString} align="center" gutterBottom style={{ fontSize: '2.0rem' }}>
-              		<strong>Rarity:</strong> {rarity}
-            	</Typography>
-            	<Typography color="textSecondary" align="center" gutterBottom style={{ fontSize: '2.0rem' }}>
-              		<strong>Symptoms:</strong> {symp}
-            	</Typography>
-            	<Typography color="textSecondary" align="center" style={{ fontSize: '2.0rem' }}>
-              		<strong>Nutrient Resource:</strong> {source}
-            	</Typography>
-          	</CardContent>
 
-		  	 <ImageList sx={{ width: 600, height: 300 }} cols={3} rowHeight={164}>
-			   	{itemData.map((item: any) => (
-					<ImageListItem key={item}>
-						<img
-							srcSet={`${item}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-							src={`${item}?w=164&h=164&fit=crop&auto=format`}
-							loading="lazy"
-						/>
-					</ImageListItem>
-				))}
-			</ImageList>
-        </Card>
-      </div>
-    </>
-  	);
+	return (
+		<>
+			<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', paddingTop: '50px' }}>
+				<Card variant="outlined" style={{ margin: 'auto', maxWidth: 600 }}>
+					<CardContent>
+						<Typography variant="h2" component="div" align="center" gutterBottom style={{ fontSize: '3.0rem' }}>
+							{nutrient}
+						</Typography>
+						<Typography color={colorString} align="center" gutterBottom style={{ fontSize: '2.0rem' }}>
+							<strong>Rarity:</strong> {rarity}
+						</Typography>
+						<Typography color="textSecondary" align="center" gutterBottom style={{ fontSize: '2.0rem' }}>
+							<strong>Symptoms:</strong> {symptoms}
+						</Typography>
+						<Typography color="textSecondary" align="center" style={{ fontSize: '2.0rem' }}>
+							<strong>Nutrient Resource:</strong> {sources}
+						</Typography>
+					</CardContent>
+
+					<ImageList sx={{ width: 600, height: 300 }} cols={3} rowHeight={164}>
+						{itemData.map((item: any) => (
+							<ImageListItem key={item}>
+								<img
+									srcSet={`${item}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+									src={`${item}?w=164&h=164&fit=crop&auto=format`}
+									loading="lazy"
+								/>
+							</ImageListItem>
+						))}
+					</ImageList>
+				</Card>
+			</div>
+		</>
+	);
 };
